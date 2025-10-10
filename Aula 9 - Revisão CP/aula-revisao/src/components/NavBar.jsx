c06851ecc8a9e6c6ad2a828e610502f174653ae9
marcossilva11 <marcosant2170@gmail.com>
2025-10-09 21:56:18 -0300

    fhsdafhsa

--- Aula 9 - Revisão CP/aula-revisao/src/components/NavBar.jsx
+++ Aula 9 - Revisão CP/aula-revisao/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <header className="bg-black text-white shadow p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">GitHub Explorer</h1>
+      <Link to='/' className="text-xl font-bold">GitHub Explorer</Link>
       <nav className="space-x-4">
         <a href="/about" className="text-gray-400 hover:underline">Sobre</a>
         <a href="/contact" className="text-gray-400 hover:underline">Contato</a>
--- Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
+++ Aula 9 - Revisão CP/aula-revisao/src/pages/Home.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  
+  const [reactRepos, setReactRepos] = useState([]);
+  const URL = import.meta.env.VITE_GITHUB_API;
+
+  useEffect(() => {
+    fetch(`${URL}node`)
+      .then((res) => res.json())
+      .then((data) => setReactRepos(data));
+  }, []);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mb-8">GitHub Repositories Explorer</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">
+        GitHub Repositories Explorer
+      </h1>
+      <p>{reactRepos.forEach(a => a)}</p>
     </div>
   );
 }
